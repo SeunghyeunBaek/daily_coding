90e52eba995c86a14eee971d2a5196a0e3521156
seunghyeunBaek <wonca14@gmail.com>
2022-01-09 14:19:01 +0900

    feat: add comment

--- 00_python_for_coding_test/8_5_fed.py
+++ 00_python_for_coding_test/8_5_fed.py
@@ -6,7 +6,17 @@ def main(input_):
     coins = [int(coin) for coin in lines[1:]]
 
     # 모르겠음 ㅜㅜ
-
+"""
+n, m = map(int, input().split())
+val = [int(input()) for _ in range(n)]
+print(val)
+d = [10001]*10001
+d[0] = 0
+for v in val:
+    for j in range(v, m+1):
+        d[j] = min(d[j], d[j-v]+1)
+print(d[m])
+"""
 if __name__ == '__main__':
     input_ = """2 15
 2
