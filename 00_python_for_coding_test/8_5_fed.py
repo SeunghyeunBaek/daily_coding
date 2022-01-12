c18d0eb88e6598d7679f70d68a9221c444f9a4d0
seunghyeunBaek <wonca14@gmail.com>
2022-01-12 19:01:11 +0900

    feat: add comment 8_5_fed.py

--- 00_python_for_coding_test/8_5_fed.py
+++ 00_python_for_coding_test/8_5_fed.py
@@ -17,6 +17,28 @@ for v in val:
         d[j] = min(d[j], d[j-v]+1)
 print(d[m])
 """
+
+"""
+https://techblog-history-younghunjo1.tistory.com/234
+import sys
+
+N, M = map(int, input().split())
+currency = [int(input()) for _ in range(N)]
+# DP 테이블
+d = [10001] * (M+1)
+d[0] = 0  # 0원일 경우
+
+for c in range(len(currency)):
+    for j in range(currency[c], M+1):
+        if d[j - currency[c]] != 10001:
+            # 화폐 단위마다 갱신
+            d[j] = min(d[j], d[j - currency[c]] + 1)
+ 
+if d[M] == 10001:
+    print(-1)
+else:
+    print(d[M])
+"""
 if __name__ == '__main__':
     input_ = """2 15
 2
