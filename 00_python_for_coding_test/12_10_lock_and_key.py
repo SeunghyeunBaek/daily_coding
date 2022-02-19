a2e0c4a5975bfddeb747ad0f75bd238eb4279ac4
Manager3 <manager3@mindsncom.onmicrosoft.com>
2022-02-19 18:08:21 +0900

    feat: wrong answer

--- 00_python_for_coding_test/12_10_lock_and_key.py
+++ 00_python_for_coding_test/12_10_lock_and_key.py
@@ -1,34 +1,62 @@
-
-
+from itertools import chain
+from itertools import product
+import time
 def unlock(key:list, lock:list)->bool:
-
-    unlocked = False
-    n_lock_rows, n_lock_cols = len(lock), len(lock[0])
-    angles = [0, 90, 180, 270]
     
-    for angle in angles:
+    flatten_lock = list(zip(*lock[::-1]))
 
-        for row_id in range(n_lock_rows):
+    if set(flatten_lock) == {1}:
+        unlocked = True
+        return unlocked
+    
+    else:        
+        unlocked = False
+        n_lock_rows, n_lock_cols = len(lock), len(lock[0])
+        n_key_rows, n_key_cols = len(key), len(key[0])
+        degrees = [0, 90, 180, 270]
+        
+        try_ = product(range(-n_key_rows+1,n_lock_rows), range(-n_key_cols+1, n_lock_cols), degrees)
 
-            for col_id in range(n_lock_cols):
+        for key_start_row_id, key_start_col_id, degree in try_:
+            rotated_key = roate_clockwise(key, degree)
+            padded_key = pad_key(rotated_key, key_start_row_id, key_start_col_id)
+            unlocked = is_unlocked(padded_key, lock)
+            
+            if unlocked:
+                break
+                    
+        return unlocked
 
-                rotated_key = roate_clockwise(key)
-                moved_key = move(rotated_key, row_id, col_id)
-                unlocked = check_unlocked(moved_key, lock)
+def roate_clockwise(key:list, degree:int)->list:
+    rotated_key = key.copy()
 
-                if unlocked:
-                    break
-                
-    return unlocked
+    for _ in range(degree//90):
+        rotated_key = list(zip(*rotated_key[::-1]))
 
-def roate_clockwise(key: list) -> list:
-    return key
+    return rotated_key
 
-def move(key: list, row_id:int, col_id: int) -> list:
-    return key
+def pad_key(key:list, key_start_row_id:int, key_start_col_id:int)->list:
+    padding = 0
+    n_key_rows, n_key_cols = len(key), len(key[0])
+    padded_key = [[padding for _ in range(n_key_cols)] for _ in range(n_key_rows)]
 
-def check_unlocked(key:list, lock:list) -> bool:
+    for row_id, col_id in product(range(n_key_rows), range(n_key_cols)):
+        refer_row_id, refer_col_id = row_id - key_start_row_id, col_id - key_start_col_id
+        is_inside = all([0 <= refer_row_id < n_key_rows, 0 <= refer_col_id < n_key_cols])
+        val = padding if not is_inside else key[refer_row_id][refer_col_id]
+        padded_key[row_id][col_id] = val
+        
+    return padded_key
+    
+def is_unlocked(key:list, lock:list)->bool:
     unlocked = False
+    flatten_key = list(chain.from_iterable(key))
+    flatten_lock = list(chain.from_iterable(lock))
+    unlock_checker = [_key+_lock for _key, _lock in zip(flatten_key, flatten_lock)]
+    
+    if set(unlock_checker) == {1}:
+        unlocked = True
+
     return unlocked
 
 if __name__ == '__main__':
